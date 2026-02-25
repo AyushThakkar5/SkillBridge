e0c7f9896bda45dfe1c72e5638e65537d9e5e590
Ayush Thakkar <a16thakkar@gmail.com>
2026-02-25 17:28:42 +0530

    check 1

--- match.py
+++ match.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/match-skills", methods=["POST"])
+@app.route("/match1-skills", methods=["POST"])
 def match_skills():
 
     data = request.get_json()
